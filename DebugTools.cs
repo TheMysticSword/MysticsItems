f6759aa682dfd13465582d98eca01bc342e6a4eb
David Galiev <galiev.david@yandex.ru>
2021-03-12 18:33:09 +0500

    Add "give monsters items" cheat to DebugTools

--- DebugTools.cs
+++ DebugTools.cs
@@ -1,32 +1,12 @@
 using RoR2;
 using R2API;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace MysticsItems
 {
     public class DebugTools
     {
-        [ConCommand(commandName = Main.TokenPrefix + "spectator", flags = ConVarFlags.Cheat, helpText = "Become invisible and remove ability to pick items up")]
-        private static void CCSpectator(ConCommandArgs args)
-        {
-            if (!enabled)
-            {
-                Main.logger.LogWarning("Debug tools are not enabled in the current build");
-                return;
-            }
-            MysticsItemsSpectator component = args.senderBody.GetComponent<MysticsItemsSpectator>();
-            if (!component) component = args.senderBody.gameObject.AddComponent<MysticsItemsSpectator>();
-            component.spectating = !component.spectating;
-            args.senderBody.modelLocator.modelTransform.GetComponentInChildren<CharacterModel>().invisibilityCount += 999 * (component.spectating ? 1 : -1);
-        }
-
-        public class MysticsItemsSpectator : MonoBehaviour
-        {
-            public bool spectating = false;
-        }
-
-        public static bool enabled = false;
-
         public static void Init()
         {
             enabled = true;
@@ -47,6 +27,75 @@ namespace MysticsItems
                 }
                 return orig(self, interactor);
             };
+
+            CharacterMaster.onStartGlobal += (master) =>
+            {
+                if (master.teamIndex == TeamIndex.Monster)
+                    foreach (KeyValuePair<ItemIndex, int> keyValuePair in monsterItems)
+                    {
+                        master.inventory.GiveItem(keyValuePair.Key, keyValuePair.Value);
+                    }
+            };
+        }
+
+        public const string ConCommandPrefix = Main.TokenPrefix + "debug_";
+        public static bool ConCommandCheck()
+        {
+            if (!enabled)
+            {
+                Main.logger.LogWarning("Debug tools are not enabled in the current build");
+                return false;
+            }
+            return true;
+        }
+
+        public class MysticsItemsSpectator : MonoBehaviour
+        {
+            public bool spectating = false;
+        }
+
+        public static bool enabled = false;
+
+        [ConCommand(commandName = ConCommandPrefix + "spectator", flags = ConVarFlags.Cheat, helpText = "Become invisible and remove ability to pick items up")]
+        private static void CCSpectator(ConCommandArgs args)
+        {
+            if (!ConCommandCheck()) return;
+            MysticsItemsSpectator component = args.senderBody.GetComponent<MysticsItemsSpectator>();
+            if (!component) component = args.senderBody.gameObject.AddComponent<MysticsItemsSpectator>();
+            component.spectating = !component.spectating;
+            args.senderBody.modelLocator.modelTransform.GetComponentInChildren<CharacterModel>().invisibilityCount += 999 * (component.spectating ? 1 : -1);
+        }
+
+        public static Dictionary<ItemIndex, int> monsterItems = new Dictionary<ItemIndex, int>();
+
+        [ConCommand(commandName = ConCommandPrefix + "givemonsters", flags = ConVarFlags.Cheat, helpText = "Give monsters an item")]
+        private static void CCGiveMonstersItem(ConCommandArgs args)
+        {
+            if (!ConCommandCheck()) return;
+            ItemIndex finalItemIndex = ItemIndex.None;
+            int count = args.Count >= 2 ? int.Parse(args[1]) : 1;
+            foreach (ItemIndex itemIndex in ItemCatalog.allItems)
+            {
+                ItemDef itemDef = ItemCatalog.GetItemDef(itemIndex);
+                if (Language.GetString(itemDef.nameToken).StartsWith(args[0]) || itemDef.name.StartsWith(args[0]))
+                {
+                    finalItemIndex = itemIndex;
+                    break;
+                }
+            }
+            if (finalItemIndex != ItemIndex.None)
+            {
+                Debug.Log("Giving enemies " + count + " " + Language.GetString(ItemCatalog.GetItemDef(finalItemIndex).nameToken));
+                if (monsterItems.ContainsKey(finalItemIndex)) monsterItems[finalItemIndex] += count;
+                else monsterItems.Add(finalItemIndex, count);
+            }
+        }
+
+        [ConCommand(commandName = ConCommandPrefix + "resetmonsteritems", flags = ConVarFlags.Cheat, helpText = "Reset monster items")]
+        private static void CCClearMonsterItems(ConCommandArgs args)
+        {
+            if (!ConCommandCheck()) return;
+            monsterItems.Clear();
         }
     }
 }
