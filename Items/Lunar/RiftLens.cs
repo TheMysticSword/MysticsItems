0e28c0705027d1fabdb45dc33e64ed3801829edc
David Galiev <galiev.david@yandex.ru>
2021-04-27 19:27:00 +0500

    Disable PostProcessDuration to prevent it from destroying the volume prematurely

--- Items/Lunar/RiftLens.cs
+++ Items/Lunar/RiftLens.cs
@@ -179,6 +179,7 @@ namespace MysticsItems.Items
             };
             ppDuration.maxDuration = 1;
             ppDuration.destroyOnEnd = true;
+            ppDuration.enabled = false;
             component.ppDuration = ppDuration;
 
             riftChestSpawnCard = ScriptableObject.CreateInstance<InteractableSpawnCard>();
