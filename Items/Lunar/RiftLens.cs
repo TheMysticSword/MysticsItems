9b298e966934d978d06419eead0cd189363dd3aa
David Galiev <galiev.david@yandex.ru>
2022-03-28 13:32:39 +0500

    Fix Rift Lens having infinite countdown

--- Items/Lunar/RiftLens.cs
+++ Items/Lunar/RiftLens.cs
@@ -429,7 +429,7 @@ namespace MysticsItems.Items
                 var distanceToNearestRift = distanceToNearestRiftForEachPlayer.Count > 0 ? distanceToNearestRiftForEachPlayer.Max() : 1f;
 
                 var totalDistance = distanceToNearestRift + distanceBetweenAllRifts;
-                var averageWalkSpeed = 7f * 1.45f * (0.25f * Run.instance.loopClearCount);
+                var averageWalkSpeed = 7f * 1.45f * (1f + 0.25f * Run.instance.loopClearCount);
                 var timeBonusFlat = 10f;
                 var calculatedCountdownTime = (totalDistance / averageWalkSpeed) + timeBonusFlat;
                 return calculatedCountdownTime;
@@ -483,7 +483,7 @@ namespace MysticsItems.Items
                 var distanceToNearestRift = distanceToNearestRiftForEachPlayer.Count > 0 ? distanceToNearestRiftForEachPlayer.Max() : 1f;
 
                 var totalDistance = distanceToNearestRift + distanceBetweenAllRifts;
-                var averageWalkSpeed = 7f * 1.45f * (0.25f * Run.instance.loopClearCount);
+                var averageWalkSpeed = 7f * 1.45f * (1f + 0.25f * Run.instance.loopClearCount);
                 // players can't always go through the shortest path due to terrain and gravity, so we'll add bonus time
                 var timeBonusMultiplier = 3f;
                 var timeBonusFlat = 20f;
