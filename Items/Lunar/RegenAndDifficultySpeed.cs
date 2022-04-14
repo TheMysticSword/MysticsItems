83bb8b3173490aed794bb11b2b0f446bb6d8fdfa
David Galiev <galiev.david@yandex.ru>
2022-04-14 17:52:36 +0500

    Change Puzzle of Chronos item display from a rigidbody to an ItemFollower

--- Items/Lunar/RegenAndDifficultySpeed.cs
+++ Items/Lunar/RegenAndDifficultySpeed.cs
@@ -6,6 +6,7 @@ using R2API.Networking.Interfaces;
 using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Rendering;
 using static MysticsItems.BalanceConfigManager;
 
 namespace MysticsItems.Items
@@ -76,19 +77,49 @@ namespace MysticsItems.Items
             HopooShaderToMaterial.Standard.Apply(material);
             ColorUtility.TryParseHtmlString("#9BFFFF", out Color color);
             HopooShaderToMaterial.Standard.Emission(material, 2f, color);
-            itemDisplayPrefab = PrepareItemDisplayModel(PrefabAPI.InstantiateClone(itemDef.pickupModelPrefab, itemDef.pickupModelPrefab.name + "Display", false));
 
-            Rigidbody rigidbody = itemDisplayPrefab.AddComponent<Rigidbody>();
-            rigidbody.useGravity = false;
+            var followerPrefab = PrefabAPI.InstantiateClone(PrepareItemDisplayModel(PrefabAPI.InstantiateClone(itemDef.pickupModelPrefab, itemDef.pickupModelPrefab.name + "Display", false)), "MysticsItems_MysticSwordItemFollowerPrefab", false);
+            followerPrefab.transform.Find("Scaler").localScale *= 0.6f;
+            ObjectTransformCurve objectTransformCurve = followerPrefab.transform.Find("Scaler").gameObject.AddComponent<ObjectTransformCurve>();
+            objectTransformCurve.useTranslationCurves = false;
+            objectTransformCurve.timeMax = 20f;
+            objectTransformCurve.rotationCurveX = AnimationCurve.Constant(0f, 1f, 0f);
+            objectTransformCurve.rotationCurveY = AnimationCurve.Linear(0f, -360f, 1f, 0f);
+            objectTransformCurve.rotationCurveY.preWrapMode = WrapMode.Loop;
+            objectTransformCurve.rotationCurveY.postWrapMode = WrapMode.Loop;
+            objectTransformCurve.rotationCurveZ = AnimationCurve.Constant(0f, 1f, 0f);
+            objectTransformCurve.useRotationCurves = true;
+            objectTransformCurve.gameObject.AddComponent<MysticsRisky2Utils.MonoBehaviours.MysticsRisky2UtilsObjectTransformCurveLoop>();
 
-            SimpleLeash leash = itemDisplayPrefab.AddComponent<SimpleLeash>();
-            leash.minLeashRadius = 1f;
-            leash.maxLeashRadius = 20f;
-            leash.maxFollowSpeed = 27f;
-            leash.smoothTime = 0.18f;
+            itemDisplayPrefab = PrefabAPI.InstantiateClone(new GameObject("MysticsItems_LunarCubeFollower"), "MysticsItems_LunarCubeFollower", false);
+            itemDisplayPrefab.AddComponent<ItemDisplay>();
+            ItemFollower itemFollower = itemDisplayPrefab.AddComponent<ItemFollower>();
+            itemFollower.followerPrefab = followerPrefab;
+            itemFollower.distanceDampTime = 0.1f;
+            itemFollower.distanceMaxSpeed = 20f;
+            itemFollower.targetObject = itemDisplayPrefab;
 
-            itemDisplayPrefab.transform.localScale *= 8f;
-            itemDisplayPrefab.transform.localRotation = itemDisplayPrefab.transform.localRotation * Quaternion.AngleAxis(45f, Vector3.up) * Quaternion.AngleAxis(45f, Vector3.forward);
+            onSetupIDRS += () =>
+            {
+                AddDisplayRule("CommandoBody", "Base", new Vector3(-0.53829F, -0.13216F, 0.11265F), new Vector3(3.15473F, 89.99998F, 270.0002F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("HuntressBody", "Base", new Vector3(-0.50487F, -0.23664F, 0.24332F), new Vector3(2.42504F, 269.9999F, 90.0001F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("Bandit2Body", "Base", new Vector3(0.17414F, -0.32998F, -0.00678F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("ToolbotBody", "Base", new Vector3(2.4828F, -4.92693F, 1.29549F), new Vector3(0F, 90F, 90F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("EngiBody", "Base", new Vector3(-0.60578F, -0.52338F, -0.14311F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("EngiTurretBody", "Base", new Vector3(-1.18285F, 2.28749F, -2.48034F), new Vector3(0F, 90F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("EngiWalkerTurretBody", "Base", new Vector3(-0.87233F, 2.00669F, -2.59369F), new Vector3(0F, 90F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("MageBody", "Base", new Vector3(-0.43306F, -0.43867F, 0.26171F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("MercBody", "Base", new Vector3(-0.52697F, -0.49787F, 0.45683F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("TreebotBody", "Base", new Vector3(-0.75802F, -1.67818F, -0.93951F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("LoaderBody", "Base", new Vector3(-0.43154F, -0.69567F, 0.22747F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("CrocoBody", "Base", new Vector3(-4.12918F, 3.9816F, 0.70414F), new Vector3(90F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("CaptainBody", "Base", new Vector3(-0.35685F, -0.47045F, 0.06453F), new Vector3(270F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("BrotherBody", "ThighL", BrotherInfection.blue, new Vector3(0.07921F, -0.00948F, 0.04758F), new Vector3(68.71732F, 228.1228F, 69.86813F), new Vector3(0.09518F, 0.04176F, 0.09524F));
+                AddDisplayRule("ScavBody", "Base", new Vector3(-14.92595F, 3.65256F, 0.84163F), new Vector3(90F, 0F, 0F), new Vector3(4F, 4F, 4F));
+                if (SoftDependencies.SoftDependenciesCore.itemDisplaysSniper) AddDisplayRule("SniperClassicBody", "Base", new Vector3(-0.5431F, 0.93526F, 0.49732F), new Vector3(10.681F, 0.007F, 0.071F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("RailgunnerBody", "Base", new Vector3(-0.59444F, -0.29985F, 0.26853F), new Vector3(90F, 0F, 0F), new Vector3(1F, 1F, 1F));
+                AddDisplayRule("VoidSurvivorBody", "Base", new Vector3(-0.41662F, 0.33464F, -0.26613F), new Vector3(90F, 0F, 0F), new Vector3(1F, 1F, 1F));
+            };
 
             RecalculateStatsAPI.GetStatCoefficients += RecalculateStatsAPI_GetStatCoefficients;
 
@@ -144,34 +175,7 @@ namespace MysticsItems.Items
                     new SyncTimer(extraDifficultyTime).Send(NetworkDestination.Clients);
                 }
                 totalItemCount = newTotalItemCount;
-
-                self.AddItemBehavior<MysticsItemsRegenAndDifficultySpeedBehaviour>(self.inventory.GetItemCount(itemDef));
             };
-        }
-
-        public class MysticsItemsRegenAndDifficultySpeedBehaviour : CharacterBody.ItemBehavior
-        {
-            public GameObject follower;
-            public SimpleLeash leash;
-
-            public void Start()
-            {
-                follower = Object.Instantiate<GameObject>(loadedDictionary["MysticsItems_RegenAndDifficultySpeed"].itemDisplayPrefab, this.body.corePosition, Quaternion.identity);
-                leash = follower.GetComponent<SimpleLeash>();
-            }
-
-            public void Update()
-            {
-                if (leash) leash.leashOrigin = body.corePosition;
-            }
-
-            public void OnDestroy()
-            {
-                if (follower)
-                {
-                    Object.Destroy(follower);
-                }
-            }
         }
 
         public class SyncTimer : INetMessage
