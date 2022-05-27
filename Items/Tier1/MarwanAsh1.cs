6caf2cacc0de74a114704ab75e074bd94822b5f9
David Galiev <galiev.david@yandex.ru>
2022-05-27 23:34:28 +0500

    Buff Marwan's Ash

--- Items/Tier1/MarwanAsh1.cs
+++ Items/Tier1/MarwanAsh1.cs
@@ -18,7 +18,7 @@ namespace MysticsItems.Items
         public static ConfigurableValue<float> damage = new ConfigurableValue<float>(
             "Item: Marwan s Ash/Light/Weapon",
             "Damage",
-            2f,
+            6f,
             "Base damage",
             new System.Collections.Generic.List<string>()
             {
@@ -30,7 +30,7 @@ namespace MysticsItems.Items
         public static ConfigurableValue<float> damagePerLevel = new ConfigurableValue<float>(
             "Item: Marwan s Ash/Light/Weapon",
             "DamagePerLevel",
-            0.2f,
+            1.2f,
             "Base damage for each additional level of the owner",
             new System.Collections.Generic.List<string>()
             {
