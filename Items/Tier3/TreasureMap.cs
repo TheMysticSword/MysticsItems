28b792330237b743c4f80a19aec907ad98f5d88e
David Galiev <galiev.david@yandex.ru>
2022-05-31 23:08:12 +0500

    Fix Treasure Map beeping from Siren Pole

--- Items/Tier3/TreasureMap.cs
+++ Items/Tier3/TreasureMap.cs
@@ -228,7 +228,7 @@ namespace MysticsItems.Items
 
         private void TeleporterInteraction_onTeleporterChargedGlobal(TeleporterInteraction teleporterInteraction)
         {
-            if (MysticsItemsTreasureMapZone.instance && MysticsItemsTreasureMapZone.instance.ShouldBeActive && NetworkServer.active)
+            if (teleporterInteraction && teleporterInteraction.enabled && MysticsItemsTreasureMapZone.instance && MysticsItemsTreasureMapZone.instance.ShouldBeActive && NetworkServer.active)
             {
                 Chat.SendBroadcastChat(new Chat.SimpleChatMessage
                 {
