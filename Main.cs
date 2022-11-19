2ef99249c4719cf6e306b7aa7f27842f7a9c6180
David Galiev <galiev.david@yandex.ru>
2022-11-19 14:11:41 +0500

    Remove leftover MysticsItems_RhythmCombo field

--- Main.cs
+++ Main.cs
@@ -340,7 +340,6 @@ namespace MysticsItems
             public static BuffDef MysticsItems_MarwanAshBurn;
             public static BuffDef MysticsItems_MarwanAshBurnStrong;
             public static BuffDef MysticsItems_MechanicalArmCharge;
-            public static BuffDef MysticsItems_RhythmCombo;
             public static BuffDef MysticsItems_SpotterMarked;
         }
 
