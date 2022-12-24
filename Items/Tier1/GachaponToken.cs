fe28569b3d09d5bf7e40f3a77e61b9bb3407a759
David Galiev <galiev.david@yandex.ru>
2022-12-25 02:08:35 +0500

    Add item displays to Gachapon and Nanomachines

--- Items/Tier1/GachaponToken.cs
+++ Items/Tier1/GachaponToken.cs
@@ -31,24 +31,24 @@ namespace MysticsItems.Items
             itemDisplayPrefab = PrepareItemDisplayModel(PrepareModel(Main.AssetBundle.LoadAsset<GameObject>("Assets/Items/Gachapon Token/FollowerModel.prefab")));
             onSetupIDRS += () =>
             {
-                AddDisplayRule("CommandoBody", "Head", new Vector3(0.109F, 0.159F, -0.123F), new Vector3(25.857F, 140.857F, 4.186F), new Vector3(0.102F, 0.102F, 0.102F));
-                AddDisplayRule("HuntressBody", "Head", new Vector3(0.077F, 0.1F, -0.13F), new Vector3(85.128F, 22.234F, 233.333F), new Vector3(0.084F, 0.084F, 0.084F));
-                AddDisplayRule("Bandit2Body", "Stomach", new Vector3(0.088F, 0.093F, 0.172F), new Vector3(72.061F, 15.327F, 356.324F), new Vector3(0.15F, 0.15F, 0.15F));
-                AddDisplayRule("ToolbotBody", "Chest", new Vector3(1.804F, 1.034F, -1.656F), new Vector3(20.492F, 214.386F, 67.265F), new Vector3(1.043F, 1.043F, 1.043F));
-                AddDisplayRule("EngiBody", "HeadCenter", new Vector3(0.064F, 0.027F, -0.169F), new Vector3(42.86F, 152.127F, 353.333F), new Vector3(0.175F, 0.175F, 0.175F));
-                AddDisplayRule("EngiTurretBody", "Head", new Vector3(0.8316F, 0.72181F, -0.23478F), new Vector3(323.0401F, 108.2313F, 5.96822F), new Vector3(0.29536F, 0.29536F, 0.29536F));
-                AddDisplayRule("EngiWalkerTurretBody", "Head", new Vector3(0.47616F, 1.3804F, -0.4785F), new Vector3(74.07391F, 268.1374F, 180F), new Vector3(0.33715F, 0.41012F, 0.3296F));
-                AddDisplayRule("MageBody", "Head", new Vector3(0.044F, 0.113F, -0.177F), new Vector3(13.198F, 167.753F, 11.941F), new Vector3(0.086F, 0.086F, 0.086F));
-                AddDisplayRule("MercBody", "Head", new Vector3(0.066F, 0.195F, -0.092F), new Vector3(358.901F, 249.659F, 64.015F), new Vector3(0.093F, 0.093F, 0.093F));
-                AddDisplayRule("TreebotBody", "PlatformBase", new Vector3(-0.073F, 0.359F, -1.062F), new Vector3(13.168F, 154.79F, 328.62F), new Vector3(0.315F, 0.315F, 0.315F));
-                AddDisplayRule("LoaderBody", "MechBase", new Vector3(0.162F, 0.046F, -0.123F), new Vector3(13.044F, 208.575F, 57.569F), new Vector3(0.15F, 0.15F, 0.15F));
-                AddDisplayRule("CrocoBody", "HandR", new Vector3(-1.129F, -1.383F, 0.659F), new Vector3(18.756F, 105.807F, 169.38F), new Vector3(1.162F, 1.162F, 1.162F));
-                AddDisplayRule("CaptainBody", "Chest", new Vector3(-0.1F, 0.226F, 0.174F), new Vector3(15.849F, 346.474F, 358.999F), new Vector3(0.086F, 0.086F, 0.086F));
-                AddDisplayRule("BrotherBody", "UpperArmL", BrotherInfection.white, new Vector3(-0.018F, 0.215F, -0.064F), new Vector3(0F, 0F, 131.256F), new Vector3(0.115F, 0.063F, 0.063F));
-                AddDisplayRule("ScavBody", "MuzzleEnergyCannon", new Vector3(-3.88535F, -0.90743F, -18.53646F), new Vector3(16.92252F, 288.3049F, 72.11835F), new Vector3(2.62999F, 2.70243F, 2.62999F));
-                if (SoftDependencies.SoftDependenciesCore.itemDisplaysSniper) AddDisplayRule("SniperClassicBody", "Chest", new Vector3(0.00102F, -0.02749F, -0.23014F), new Vector3(7.74695F, 179.5125F, 359.482F), new Vector3(0.09241F, 0.09241F, 0.09241F));
-                AddDisplayRule("RailgunnerBody", "BottomRail", new Vector3(0.00312F, 0.36536F, -0.04365F), new Vector3(357.5628F, 151.3911F, 256.462F), new Vector3(0.15615F, 0.15615F, 0.15615F));
-                AddDisplayRule("VoidSurvivorBody", "Neck", new Vector3(0.10783F, 0.10931F, -0.151F), new Vector3(65.45281F, 129.4731F, 339.067F), new Vector3(0.1282F, 0.1282F, 0.1282F));
+                AddDisplayRule("CommandoBody", "Stomach", new Vector3(-0.08186F, 0.05079F, 0.1678F), new Vector3(324.9999F, 245.9885F, 279.2025F), new Vector3(0.03735F, 0.03735F, 0.03735F));
+                AddDisplayRule("HuntressBody", "Stomach", new Vector3(-0.12788F, 0.04712F, 0.14654F), new Vector3(276.4182F, 0.00003F, 325.6896F), new Vector3(0.03684F, 0.03684F, 0.03684F));
+                AddDisplayRule("Bandit2Body", "Stomach", new Vector3(-0.13064F, 0.04437F, 0.15777F), new Vector3(83.75449F, 309.2176F, 343.5378F), new Vector3(0.02754F, 0.02754F, 0.02754F));
+                AddDisplayRule("ToolbotBody", "Chest", new Vector3(-2.40704F, 1.7756F, 1.887F), new Vector3(0F, 0F, 90F), new Vector3(0.45775F, 0.45775F, 0.45775F));
+                AddDisplayRule("EngiBody", "Stomach", new Vector3(-0.11678F, -0.018F, 0.1798F), new Vector3(82.83242F, 318.2914F, 341.584F), new Vector3(0.04024F, 0.04024F, 0.04024F));
+                //AddDisplayRule("EngiTurretBody", "Head", new Vector3(0.8316F, 0.72181F, -0.23478F), new Vector3(323.0401F, 108.2313F, 5.96822F), new Vector3(0.29536F, 0.29536F, 0.29536F));
+                //AddDisplayRule("EngiWalkerTurretBody", "Head", new Vector3(0.47616F, 1.3804F, -0.4785F), new Vector3(74.07391F, 268.1374F, 180F), new Vector3(0.33715F, 0.41012F, 0.3296F));
+                AddDisplayRule("MageBody", "Pelvis", new Vector3(-0.10756F, -0.02973F, -0.1595F), new Vector3(276.6097F, 268.12F, 113.7062F), new Vector3(0.03137F, 0.03137F, 0.03137F));
+                AddDisplayRule("MercBody", "ThighL", new Vector3(0.00301F, -0.04705F, 0.19166F), new Vector3(75.6413F, 326.5565F, 301.1101F), new Vector3(0.04367F, 0.04367F, 0.04367F));
+                AddDisplayRule("TreebotBody", "HeadCenter", new Vector3(0F, 0.04031F, 0F), new Vector3(0F, 0F, 0F), new Vector3(0.06867F, 0.06867F, 0.06867F));
+                AddDisplayRule("LoaderBody", "Pelvis", new Vector3(-0.10359F, 0.07784F, -0.18451F), new Vector3(80.79361F, 0F, 0F), new Vector3(0.03828F, 0.03828F, 0.03828F));
+                AddDisplayRule("CrocoBody", "Head", new Vector3(1.51614F, 1.86602F, 0.39052F), new Vector3(7.85445F, 337.2328F, 288.036F), new Vector3(0.17627F, 0.17743F, 0.17627F));
+                AddDisplayRule("CaptainBody", "Chest", new Vector3(0.16838F, 0.25327F, 0.15011F), new Vector3(284.663F, 240.0974F, 150.5083F), new Vector3(0.03817F, 0.03817F, 0.03817F));
+                AddDisplayRule("BrotherBody", "Stomach", new Vector3(-0.12494F, 0.15612F, 0.07713F), new Vector3(0F, 48.36884F, 0F), new Vector3(0.03742F, 0.03184F, 0.03184F));
+                //AddDisplayRule("ScavBody", "MuzzleEnergyCannon", new Vector3(-3.88535F, -0.90743F, -18.53646F), new Vector3(16.92252F, 288.3049F, 72.11835F), new Vector3(2.62999F, 2.70243F, 2.62999F));
+                if (SoftDependencies.SoftDependenciesCore.itemDisplaysSniper) AddDisplayRule("SniperClassicBody", "Pelvis", new Vector3(0.08531F, 0.20379F, 0.19368F), new Vector3(84.48428F, -0.00001F, 174.7452F), new Vector3(0.0399F, 0.0399F, 0.0399F));
+                AddDisplayRule("RailgunnerBody", "Pelvis", new Vector3(0.10035F, 0.06732F, -0.09966F), new Vector3(68.92004F, 321.2874F, 354.5732F), new Vector3(0.04588F, 0.04588F, 0.04588F));
+                AddDisplayRule("VoidSurvivorBody", "Head", new Vector3(-0.09242F, -0.05035F, -0.11544F), new Vector3(317.6919F, 150.0517F, 240.3664F), new Vector3(0.03197F, 0.03197F, 0.03285F));
             };
 
             ShrineChanceBehavior.onShrineChancePurchaseGlobal += ShrineChanceBehavior_onShrineChancePurchaseGlobal;
--- Items/Tier2/Nanomachines.cs
+++ Items/Tier2/Nanomachines.cs
@@ -55,24 +55,24 @@ namespace MysticsItems.Items
             itemDisplayPrefab = PrepareItemDisplayModel(PrepareModel(Main.AssetBundle.LoadAsset<GameObject>("Assets/Items/Proximity Nanobots/FollowerModel.prefab")));
             onSetupIDRS += () =>
             {
-                AddDisplayRule("CommandoBody", "LowerArmR", new Vector3(0.001F, 0.274F, -0.078F), new Vector3(7.29F, 186.203F, 0.157F), new Vector3(0.277F, 0.389F, 0.277F));
-                AddDisplayRule("HuntressBody", "HandL", new Vector3(-0.014F, 0.004F, 0.035F), new Vector3(6.909F, 1.748F, 74.816F), new Vector3(0.187F, 0.174F, 0.187F));
-                AddDisplayRule("Bandit2Body", "Stomach", new Vector3(-0.069F, -0.12F, -0.197F), new Vector3(18.152F, 14.491F, 196.624F), new Vector3(0.348F, 0.348F, 0.348F));
-                AddDisplayRule("ToolbotBody", "HandR", new Vector3(-0.059F, 0.587F, 1.939F), new Vector3(356.736F, 85.148F, 90.496F), new Vector3(3.014F, 3.241F, 3.014F));
-                AddDisplayRule("EngiBody", "HandL", new Vector3(0F, 0.104F, 0.042F), new Vector3(3.001F, 0F, 0F), new Vector3(0.259F, 0.259F, 0.259F));
-                AddDisplayRule("EngiTurretBody", "Head", new Vector3(0.026F, 0.602F, -1.541F), new Vector3(22.044F, 48.281F, 206.737F), new Vector3(0.74F, 0.74F, 0.74F));
-                AddDisplayRule("EngiWalkerTurretBody", "Head", new Vector3(-0.248F, 1.434F, -0.84F), new Vector3(300.601F, 223.502F, 297.144F), new Vector3(0.659F, 0.801F, 0.643F));
-                AddDisplayRule("MageBody", "HandL", new Vector3(-0.011F, 0.074F, 0.104F), new Vector3(0F, 0F, 355.462F), new Vector3(0.22F, 0.22F, 0.22F));
-                AddDisplayRule("MercBody", "HandR", new Vector3(0F, 0.112F, 0.103F), new Vector3(14.285F, 0F, 0F), new Vector3(0.427F, 0.427F, 0.427F));
-                AddDisplayRule("TreebotBody", "WeaponPlatform", new Vector3(0F, 0.889F, 0.308F), new Vector3(0F, 0F, 0F), new Vector3(0.846F, 0.846F, 0.846F));
-                AddDisplayRule("LoaderBody", "MechHandL", new Vector3(-0.073F, 0.379F, 0.15F), new Vector3(5.558F, 330.424F, 0F), new Vector3(0.36F, 0.36F, 0.36F));
-                AddDisplayRule("CrocoBody", "HandL", new Vector3(-1.286F, 0.394F, 0.102F), new Vector3(56.075F, 280.047F, 0F), new Vector3(3.614F, 2.545F, 4.003F));
-                AddDisplayRule("CaptainBody", "HandR", new Vector3(-0.086F, 0.125F, 0.016F), new Vector3(14.676F, 274.88F, 359.215F), new Vector3(0.248F, 0.248F, 0.248F));
-                AddDisplayRule("BrotherBody", "HandL", BrotherInfection.green, new Vector3(0.019F, -0.013F, 0.017F), new Vector3(348.105F, 324.594F, 242.165F), new Vector3(0.061F, 0.019F, 0.061F));
-                AddDisplayRule("ScavBody", "HandL", new Vector3(-3.491F, 2.547F, -2.4F), new Vector3(354.216F, 329.486F, 87.688F), new Vector3(7.501F, 7.7F, 7.501F));
-                if (SoftDependencies.SoftDependenciesCore.itemDisplaysSniper) AddDisplayRule("SniperClassicBody", "LowerArmR", new Vector3(-0.00116F, 0.35526F, -0.02252F), new Vector3(12.00797F, 359.9336F, 355.1882F), new Vector3(0.2754F, 0.2754F, 0.2754F));
-                AddDisplayRule("RailgunnerBody", "Pelvis", new Vector3(0.1939F, 0.23069F, 0.00249F), new Vector3(5.45999F, 119.7464F, 17.75949F), new Vector3(0.33522F, 0.33522F, 0.33522F));
-                AddDisplayRule("VoidSurvivorBody", "Head", new Vector3(-0.06693F, -1.12064F, -0.77318F), new Vector3(90F, 0F, 0F), new Vector3(0.5795F, 0.5795F, 0.5795F));
+                AddDisplayRule("CommandoBody", "CalfR", new Vector3(-0.02165F, 0.14397F, -0.0533F), new Vector3(283.8492F, 276.3043F, 98.7495F), new Vector3(0.02579F, 0.02579F, 0.02579F));
+                AddDisplayRule("HuntressBody", "CalfR", new Vector3(0.06795F, 0.2092F, -0.04452F), new Vector3(278.7839F, 1.2873F, 327.3282F), new Vector3(0.0243F, 0.0243F, 0.0243F));
+                AddDisplayRule("Bandit2Body", "CalfR", new Vector3(-0.00552F, 0.18029F, -0.06738F), new Vector3(313.3941F, 92.65309F, 275.5553F), new Vector3(0.02547F, 0.02547F, 0.02547F));
+                AddDisplayRule("ToolbotBody", "CalfR", new Vector3(0.62627F, 2.21605F, 0.05282F), new Vector3(28.88263F, 6.02953F, 270.3149F), new Vector3(0.21708F, 0.21708F, 0.21708F));
+                AddDisplayRule("EngiBody", "CalfR", new Vector3(0.0164F, 0.12436F, -0.08442F), new Vector3(277.2202F, 180F, 176.1767F), new Vector3(0.02989F, 0.02989F, 0.02989F));
+                AddDisplayRule("EngiTurretBody", "LegBar3", new Vector3(0.02396F, 0.35298F, 0.18667F), new Vector3(6.86833F, 93.31924F, 90.39735F), new Vector3(0.08437F, 0.08437F, 0.08437F));
+                AddDisplayRule("EngiWalkerTurretBody", "LegBar3", new Vector3(0.00002F, 0.17597F, 0.2548F), new Vector3(62.81832F, 90.86576F, 90.97321F), new Vector3(0.102F, 0.102F, 0.102F));
+                AddDisplayRule("MageBody", "CalfR", new Vector3(0.0241F, 0.02332F, -0.03891F), new Vector3(270.3604F, 180.0011F, 148.1224F), new Vector3(0.02423F, 0.02423F, 0.02423F));
+                AddDisplayRule("MercBody", "CalfR", new Vector3(0.00001F, 0.20225F, -0.03167F), new Vector3(273.8359F, 180F, 180F), new Vector3(0.0241F, 0.0241F, 0.0241F));
+                AddDisplayRule("TreebotBody", "Base", new Vector3(0.5353F, 0.5163F, 0.3298F), new Vector3(3.67085F, 335.3158F, 286.064F), new Vector3(0.05916F, 0.05916F, 0.05916F));
+                AddDisplayRule("LoaderBody", "CalfR", new Vector3(0.0098F, 0.11872F, -0.09638F), new Vector3(355.0381F, 82.86241F, 270.4088F), new Vector3(0.02775F, 0.02775F, 0.02775F));
+                AddDisplayRule("CrocoBody", "Head", new Vector3(0.03485F, 3.12425F, 1.55491F), new Vector3(357.2307F, 270.6913F, 255.9768F), new Vector3(0.38331F, 0.38331F, 0.38331F));
+                AddDisplayRule("CaptainBody", "CalfR", new Vector3(0F, 0.26157F, -0.03875F), new Vector3(359.5825F, 90.05307F, 262.7561F), new Vector3(0.02424F, 0.02424F, 0.02424F));
+                AddDisplayRule("BrotherBody", "CalfR", BrotherInfection.green, new Vector3(-0.02875F, 0.09234F, -0.01974F), new Vector3(0F, 297.8474F, 0F), new Vector3(0.05206F, 0.05206F, 0.05206F));
+                AddDisplayRule("ScavBody", "ThighR", new Vector3(-0.65041F, 1.77553F, 3.10771F), new Vector3(338.4114F, 221.8671F, 314.6543F), new Vector3(0.61603F, 0.61603F, 0.61603F));
+                if (SoftDependencies.SoftDependenciesCore.itemDisplaysSniper) AddDisplayRule("SniperClassicBody", "CalfR", new Vector3(0F, 0.11285F, 0.09567F), new Vector3(65.87368F, 90.73203F, 90.80209F), new Vector3(0.02974F, 0.02974F, 0.02974F));
+                AddDisplayRule("RailgunnerBody", "CalfR", new Vector3(-0.08793F, 0.21366F, -0.00001F), new Vector3(0F, 0F, 67.67302F), new Vector3(0.02516F, 0.02516F, 0.02516F));
+                AddDisplayRule("VoidSurvivorBody", "CalfR", new Vector3(-0.08759F, 0.07466F, -0.00465F), new Vector3(326.4437F, 324.9552F, 104.0954F), new Vector3(0.02294F, 0.02294F, 0.02294F));
             };
 
             var mat = itemDef.pickupModelPrefab.GetComponentInChildren<Renderer>().sharedMaterial;
@@ -110,6 +110,7 @@ namespace MysticsItems.Items
                             if (!attachedEffect)
                             {
                                 attachedEffect = Instantiate(activeEffectPrefab, body.coreTransform);
+                                attachedEffect.transform.localScale = Vector3.one * 2f * body.radius;
                             }
                             foreach (var ps in attachedEffect.GetComponentsInChildren<ParticleSystem>())
                             {
