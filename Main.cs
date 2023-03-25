0cfc0c7a8047653529cfc8f556ab6de2ac0baa59
David Galiev <galiev.david@yandex.ru>
2023-03-25 21:27:27 +0500

    Update version and changelog

--- Main.cs
+++ Main.cs
@@ -37,7 +37,7 @@ namespace MysticsItems
     {
         public const string PluginGUID = "com.themysticsword.mysticsitems";
         public const string PluginName = "MysticsItems";
-        public const string PluginVersion = "2.1.6";
+        public const string PluginVersion = "2.1.7";
 
         internal static BepInEx.Logging.ManualLogSource logger;
         internal static PluginInfo pluginInfo;
